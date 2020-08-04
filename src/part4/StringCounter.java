71d1bee6bec8c1cf53efda96403e07415fe41631
Kar-Den <k.denis.vlad@gmail.com>
2020-08-05 01:10:36 +0300

    Part4.task 1 начал

---
+++ src/part4/StringCounter.java
@@ -0,0 +1,11 @@
+package part4;
+
+import java.util.Scanner;
+
+public class StringCounter {
+    public static void main(String[] args) {
+        Scanner cs = new Scanner(System.in);
+        String str = cs.nextLine();
+//        System.out.println(str);
+    }
+}
