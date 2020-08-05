7b6cb1aba9ad2810bde180b828fb53f3be7eba71
Kar-Den <k.denis.vlad@gmail.com>
2020-08-06 02:00:32 +0300

    Part4.task 1 Написать консольное приложение для обработки строки принятой от пользователя по описанным ниже условиям:              - подсчитать и вывести количество слов в полученной от пользователя строке (* предлоги считать как отдельное слово)              - вывести на экран с самое длинное и самое короткое слово              - запросить у пользователя букву и символ, на который данная буква будет заменена, произвести замену запрошенной буквы на символ по всей строке
                 	буква = О; символ - *
                 	исходная строка - "первоеСлово второеСлово третьеСлово четвертоеСлово пятоеСлово"
                 	result - "перв*еСл*в* вт*р*еСл*в* третьеСл*в* четверт*еСл*в* пят*еСл*в*"

--- src/part4/StringCounter.java
+++ src/part4/StringCounter.java
@@ -6,18 +6,58 @@ import java.util.regex.Pattern;
 
 public class StringCounter {
     public static void main(String[] args) {
+        System.out.println("Enter the string:");
         Scanner cs = new Scanner(System.in);
         String str = cs.nextLine();
-//        System.out.println(str);
 
         Pattern pattern = Pattern.compile("[ёЁа-яА-Яa-zA-Z]+");
         Matcher matcher = pattern.matcher(str);
 
         int nWorlds = 0;
-        while (matcher.find()){
+        String[] shortWorld = str.split("\\b");
+        int numberLettersShortWorld = shortWorld[0].length();
+
+        while (matcher.find()) {
+            int timeAmtShort = matcher.group().length();
+            if (timeAmtShort < numberLettersShortWorld) {
+                numberLettersShortWorld = timeAmtShort;
+                shortWorld[0] = matcher.group();
+            }
             nWorlds++;
         }
 
-        System.out.println(nWorlds + " слов");
+        matcher.reset();
+
+        String[] longWorld = str.split("\\b");
+        int numberLettersLongWorld = longWorld[0].length();
+
+        while (matcher.find()) {
+            int timeAmtLong = matcher.group().length();
+            if (timeAmtLong > numberLettersLongWorld) {
+                numberLettersLongWorld = timeAmtLong;
+                longWorld[0] = matcher.group();
+            }
+        }
+
+
+        System.out.println("--> In text " + nWorlds + " worlds");
+        System.out.println("--> shortest world: " + shortWorld[0]);
+        System.out.println("--> longest world: " + longWorld[0]);
+        System.out.println();
+
+        System.out.print("which letter will replace? ");
+        Scanner scanner = new Scanner(System.in);
+        char letter = scanner.next().charAt(0);
+
+        System.out.print("what character? ");
+        Scanner keyboard = new Scanner(System.in);
+        char character = keyboard.next().charAt(0);
+        String result = str.replace(letter, character);
+
+        System.out.print("\tletter = " + letter + ";");
+        System.out.println(" character - " + character);
+        System.out.println("\tsource string - \"" + str + "\"");
+
+        System.out.println("\tresult - \"" + result + "\"");
     }
 }
