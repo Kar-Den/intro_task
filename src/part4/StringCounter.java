10fd540b76a087477eac61ec860495b815a8baf0
Kar-Den <k.denis.vlad@gmail.com>
2020-08-05 03:46:25 +0300

    Part4.task 1 считает количество слов

--- src/part4/StringCounter.java
+++ src/part4/StringCounter.java
@@ -1,11 +1,23 @@
 package part4;
 
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class StringCounter {
     public static void main(String[] args) {
         Scanner cs = new Scanner(System.in);
         String str = cs.nextLine();
 //        System.out.println(str);
+
+        Pattern pattern = Pattern.compile("[ёЁа-яА-Яa-zA-Z]+");
+        Matcher matcher = pattern.matcher(str);
+
+        int nWorlds = 0;
+        while (matcher.find()){
+            nWorlds++;
+        }
+
+        System.out.println(nWorlds + " слов");
     }
 }
